fcfeaaed980fbe0dcb2fb42de2db466467d4f434
lhas <luizhrqas@gmail.com>
2016-08-05 04:46:43 -0300

    botoes de idioma e angular-translate carregado :+1:

--- src/app/header.js
+++ src/app/header.js
@@ -1,5 +1,23 @@
-angular
-  .module('app')
-  .component('fountainHeader', {
-    templateUrl: 'app/header.html'
-  });
+/* header.js */
+(function(){
+  'use strict';
+
+  angular
+    .module('app')
+    .component('fountainHeader', {
+      templateUrl: 'app/header.html',
+      controller: HeaderController
+    });
+
+    HeaderController.$inject = [];
+    function HeaderController() {
+      var vm = this;
+
+      vm.changeLanguage = changeLanguage;
+      
+      /* Esta função modifica o idioma do nosso app. */
+      function changeLanguage(locale) {
+        alert('Você selecionou o idioma: ' + locale);
+      }
+    }
+})();
